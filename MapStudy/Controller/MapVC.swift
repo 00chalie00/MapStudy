dd55c6860b96b411bc0a594042ddd8fc9234700a
Lee Chul <winfe79@hanmail.net>
2019-05-30 17:40:20 +0900

    initUI

---
+++ MapStudy/Controller/MapVC.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  MapStudy
+//
+//  Created by formathead on 30/05/2019.
+//  Copyright © 2019 formathead. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapVC: UIViewController {
+
+    //Outlet
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func MapCenterPressed(_ sender: Any) {
+        
+    }
+    
+}//End Of The Class
+
+
---
+++ Podfile
@@ -0,0 +1,21 @@
+platform :ios, '9.0'
+
+target 'MapStudy' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MapStudy
+    pod 'Alamofire'
+    pod 'AlamofireImage'
+
+  target 'MapStudyTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'MapStudyUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
